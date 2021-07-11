174c16cbcf62bb3b051080969bf7d9feeee1dd35
reidjs <reidsherman@gmail.com>
2021-07-11 16:56:30 -0700

    fix infinite loop

--- pronouns_to_words.py
+++ pronouns_to_words.py
@@ -13,16 +13,10 @@ for pronoun in raw_pronouns:
     p = pronoun.strip()
     pronouns.append(p)
 
-ig_words = {}
-for L in range(0, len(pronouns) + 1):
+for L in range(1, 4):
     for subset in itertools.combinations(pronouns, L):
-        for M in range(1, len(subset) + 1):
-            for subset2 in itertools.combinations(subset, M):
-                joined_word = "".join(subset2)
-                lookup = dictionary.get(joined_word)
-                # NOTE: unfortunately this algorithm generates a lots of duplicates
-                already_exists = ig_words.get(joined_word)
-                if (lookup and not already_exists):
-                    ig_words[joined_word] = True
-                    sys.stdout.write(joined_word)
-
+        joined_word = "".join(subset)
+        lookup = dictionary.get(joined_word)
+        if (lookup):
+            sys.stdout.write(joined_word)
+            sys.stdout.write("\n")
